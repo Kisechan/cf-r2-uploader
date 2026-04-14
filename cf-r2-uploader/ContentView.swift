5c032528f6ade56fb64e1cc44c16b6f5d740234e
Kisechan <kisechan_@outlook.com>
2026-04-14 18:28:58 +0800

    Add initial project files and configuration for SwiftUI app

---
+++ cf-r2-uploader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cf-r2-uploader
+//
+//  Created by Kise Platinyl on 2026/4/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
